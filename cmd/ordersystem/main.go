ea8a2998373b7a4fa024355db27ef64196c4d120
marcosvlima <marcoslima.xv@gmail.com>
2025-10-04 14:18:02 -0300

    Webserver and main

---
+++ cmd/ordersystem/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/marcosvlima/clean-arch-go-sample/configs"
+	"github.com/marcosvlima/clean-arch-go-sample/internal/event/handler"
+	"github.com/marcosvlima/clean-arch-go-sample/internal/infra/web/webserver"
+	"github.com/marcosvlima/clean-arch-go-sample/pkg/events"
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	configs, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	db, err := sql.Open(configs.DBDriver, fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", configs.DBUser, configs.DBPassword, configs.DBHost, configs.DBPort, configs.DBName))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	rabbitMQChannel := getRabbitMQChannel()
+
+	eventDispatcher := events.NewEventDispatcher()
+	eventDispatcher.Register("OrderCreated", &handler.OrderCreatedHandler{
+		RabbitMQChannel: rabbitMQChannel,
+	})
+
+	// webserver
+	webserver := webserver.NewWebServer(configs.WebServerPort)
+	webOrderHandler := NewWebOrderHandler(db, eventDispatcher)
+	webserver.AddHandler("/order", webOrderHandler.Create)
+	fmt.Println("Starting web server on port", configs.WebServerPort)
+	go webserver.Start()
+}
+
+func getRabbitMQChannel() *amqp.Channel {
+	conn, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672/")
+	if err != nil {
+		panic(err)
+	}
+	ch, err := conn.Channel()
+	if err != nil {
+		panic(err)
+	}
+	return ch
+}
